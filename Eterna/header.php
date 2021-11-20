426817b458b50656c8fdf3928b7acc1ebb09ff95
WeijingD <dongweijing49@gmail.com>
2021-11-20 12:58:56 -0800

    Edit Nav bar name
    change from "Portfolio" to "Gallary"

--- Eterna/header.php
+++ Eterna/header.php
@@ -10,7 +10,7 @@
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="active" href="index.php">Home</a></li>
-          <li><a href="portfolio.php">Portfolio</a></li>
+          <li><a href="portfolio.php">Gallary</a></li>
           <li class="dropdown"><a href="#"><span>My account</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Login</a></li>
