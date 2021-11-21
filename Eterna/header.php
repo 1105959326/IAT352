012db14875800a7eb429f1295e8bc570f3ca3cd2
Andrew <laz1105959326@gmail.com>
2021-11-20 16:28:44 -0800

    galleryupdate

--- Eterna/header.php
+++ Eterna/header.php
@@ -1,4 +1,31 @@
-<header id="header" class="d-flex align-items-center">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Vancouver Public Art</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../Eterna/assets/img/favicon.png" rel="icon">
+  <link href="../Eterna/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../Eterna/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="../Eterna/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../Eterna/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../Eterna/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../Eterna/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../Eterna/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../Eterna/assets/css/style.css" rel="stylesheet"><header id="header" class="d-flex align-items-center">
     <div class="container d-flex justify-content-between align-items-center">
 
       <div class="logo">
--- public/gallery.php
+++ public/gallery.php
@@ -1,8 +1,52 @@
 <?php
 
-require_once('../private/initialize.php');
-$res = queryAll('artwork');
-while ($row = mysqli_fetch_assoc($res)){
-	echo $row['RegistryID'];
-}
+
 ?>
+
+<?php require_once('../Eterna/header.php'); ?>
+
+<main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>Gallery</li>
+        </ol>
+        <h2>Gallery</h2>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+        <section id="portfolio" class="portfolio">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">All</li>
+              <li data-filter=".filter-app">App</li>
+              <li data-filter=".filter-card">Card</li>
+              <li data-filter=".filter-web">Web</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container">
+
+
+        <?php
+        require_once('../private/initialize.php');
+		$res = queryAll('artwork');
+		while ($row = mysqli_fetch_assoc($res)){
+		    echo "<div class=\"col-lg-4 col-md-6 portfolio-item filter-app\">";
+		    echo "<div class=\"portfolio-wrap\">";
+		    echo "<img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">";
+
+		    }
+
+        ?>
+
+
