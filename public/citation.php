b65f48df6494b3e34fe093ea814119fa3cc0791a
WeijingD <dongweijing49@gmail.com>
2021-11-25 20:50:43 -0800

    Citation

--- public/citation.php
+++ public/citation.php
@@ -31,10 +31,10 @@
       <div class="container">
 
         <ol>
-          <li><a href="index.html">Home</a></li>
-          <li>Contact</li>
+          <li><a href="index.php">Home</a></li>
+          <li>Citation</li>
         </ol>
-        <h2>Contact</h2>
+        <h2>Citation</h2>
 
       </div>
     </section><!-- End Breadcrumbs -->
@@ -46,65 +46,48 @@
         <div class="row">
           <div class="col-lg-6">
             <div class="info-box mb-4">
-              <i class="bx bx-map"></i>
-              <h3>Our Address</h3>
-              <p>A108 Adam Street, New York, NY 535022</p>
+              <h3>Bootstrap</h3>
+              <p>Template Name: Eterna 
+              <br>Template URL: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/
+              <br>Author: BootstrapMade.com
+              <br>License: https://bootstrapmade.com/license/</p>
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-6">
-            <div class="info-box  mb-4">
-              <i class="bx bx-envelope"></i>
-              <h3>Email Us</h3>
-              <p>contact@example.com</p>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6">
-            <div class="info-box  mb-4">
-              <i class="bx bx-phone-call"></i>
-              <h3>Call Us</h3>
-              <p>+1 5589 55488 55</p>
-            </div>
-          </div>
-
-        </div>
-
-        <div class="row">
-
-          <div class="col-lg-6 ">
-            <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD-LdeoPQejHASho9UBQNvAlvPrBS6zwVM&q=49.2730833,-123.1049943" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
-          </div>
-
+          <div class="row">
           <div class="col-lg-6">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-                </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                </div>
-              </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
+            <div class="info-box mb-4">
+              <h3>DataBase</h3>
+              <p>https://opendata.vancouver.ca/explore/dataset/public-art/information/
+              <br>License: https://opendata.vancouver.ca/pages/licence/
+              <br>Publisher: City of Vancouver</p>
+            </div>
           </div>
 
+          <div class="row">
+          <div class="col-lg-6">
+            <div class="info-box mb-4">
+              <h3>IAT 352 Final Project</h3>
+              <p>Instructors: Marek Hatala 
+              <br>TA: Leo Ruan
+              <br>Simon Fraser University
+              <br>Fall 2021</p>
+            </div>
+          </div>
+
+          <div class="row">
+          <div class="col-lg-6">
+            <div class="info-box mb-4">
+              <h3>Team</h3>
+              <p>Andrew Lin & Weijing Dong </p>
+            </div>
+          </div>
+
+          
         </div>
 
       </div>
-    </section><!-- End Contact Section -->
+    </section>
 
   </main><!-- End #main -->
 
