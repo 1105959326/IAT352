70e5d205ab6966e0f98859e100ed0d73815b7818
Andrew <laz1105959326@gmail.com>
2021-12-11 23:47:57 -0800

    Update ajax.js

--- private/ajax.js
+++ private/ajax.js
@@ -37,11 +37,11 @@ $(document).ready(function(){
     $('.common_selector').click(function(){
         filter_data();
     });
-
+    //create function to read parameter from url
     function getUrlParam(name) {
-            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-            var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-            if (r != null) return unescape(r[2]); return null; //返回参数值
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); 
+            var r = window.location.search.substr(1).match(reg);  
+            if (r != null) return unescape(r[2]); return null;
     }
 
 
